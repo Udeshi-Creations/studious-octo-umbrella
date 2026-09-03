a020cca3005b06a8bc1ab4ebb626c05388849734
Prem Udeshi <premudeshi99@gmail.com>
2026-09-03 00:48:50 -0400

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,15 +2,14 @@ group = "com.example.teelink_activity_launcher"
 version = "1.0-SNAPSHOT"
 
 buildscript {
-    val kotlinVersion = "2.4.0"
     repositories {
         google()
         mavenCentral()
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:9.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 
@@ -21,57 +20,24 @@ allprojects {
     }
 }
 
-plugins {
-    id("com.android.library")
-}
+apply(plugin = "com.android.library")
+apply(plugin = "kotlin-android")
 
 android {
     namespace = "com.example.teelink_activity_launcher"
 
-    compileSdk = 36
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    sourceSets {
-        getByName("main") {
-            java.srcDirs("src/main/kotlin")
-        }
-        getByName("test") {
-            java.srcDirs("src/test/kotlin")
-        }
+    kotlinOptions {
+        jvmTarget = "17"
     }
-
-    defaultConfig {
-        minSdk = 24
-    }
-
-    testOptions {
-        unitTests {
-            isIncludeAndroidResources = true
-            all {
-                it.useJUnitPlatform()
-
-                it.outputs.upToDateWhen { false }
-
-                it.testLogging {
-                    events("passed", "skipped", "failed", "standardOut", "standardError")
-                    showStandardStreams = true
-                }
-            }
-        }
-    }
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
-    }
-}
-
-dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.mockito:mockito-core:5.0.0")
 }
